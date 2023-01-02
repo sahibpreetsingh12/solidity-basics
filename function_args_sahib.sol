790a11536963a01967edf0b08be375785856bbba
sahibpreet singh <ss9334931@gmail.com>
2023-01-02 18:17:42 +0530

    How to pass arguments ina function
    Here in this file we have just made a minor change in the function where we have shown how to pass arguments in the function

---
+++ function_args_sahib.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract identity {
+    string name;
+    uint age;
+
+    constructor()  // similar to constructors in other languages
+    {
+        name="sahib";
+        age = 25;
+    }
+
+    function getname() view public returns (string memory)
+    {
+        return name;
+    }
+
+    function getage() view public returns (uint)
+    {
+        return age;
+    }
+
+    function setage(uint new_age) public{ // pasiing arguments in the function
+
+
+        age = age+new_age+2;
+        
+    }
+}
